80a1184651be80c1c3e3848e8c52e30e69c7f94a
HuangjieJin <jinhuangjie233@gmail.com>
2021-03-23 16:03:08 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(stringNames, eventName) {
+    for(let i=0; i<stringNames.length; i+=1){
+        stringNames[i] = `Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`
+    }
+    return stringNames;
+}
+
+countDown = number => {
+    while (number>=0) {
+        console.log(number);
+        number-=1;
+    }
+}
